39f9372b7cc0a08042dd835b259462985c425a10
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2021-08-06 23:13:54 +0530

    Create Assignment 1.4.py

---
+++ DAY-1/Assignment 1.4.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+#below is one way of swapping
+#a,b=b,a
+
+
+#another traditional way
+temp=a
+a=b
+b=temp
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
